57536cecbccf86584694a1ae011a73334abfcf81
Karim Khaleel <karim.abouzeid01@gmail.com>
2024-10-03 13:05:36 +0300

    Fixed a bug

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,43 +1,36 @@
 import { Component } from '@angular/core';
-import {User} from "../models/user";
-import {UserService} from "../services/user/user.service";
-import {FormsModule} from "@angular/forms";
-import {MatButtonModule} from "@angular/material/button";
-import {MatInputModule} from "@angular/material/input";
-import {MatFormFieldModule} from "@angular/material/form-field";
+import { User } from '../models/userDto';
+import { UserService } from '../services/user/user.service';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-add-user',
   standalone: true,
-  imports: [
-    FormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule
-  ],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './add-user.component.html',
-  styleUrl: './add-user.component.css'
+  styleUrl: './add-user.component.css',
 })
-
 export class AddUserComponent {
-
   newUser: User = {
     firstName: '',
     lastName: '',
     email: '',
-    password: "",
-    phone: ""
+    password: '',
+    phone: '',
   };
 
   constructor(private userService: UserService) {}
 
   addUser(): void {
     this.userService.addUser(this.newUser).subscribe(
-      response => {
+      (response) => {
         console.log('User added successfully:', response);
         // Handle success
       },
-      error => {
+      (error) => {
         console.error('Error adding user:', error);
         // Handle error
       }
